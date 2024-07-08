261b9abf1b181f1c226e4efe9006e135d8020ee6
SkyNet-PKN <SkyNet0PKN@gmail.com>
2024-07-08 19:13:33 +0800

    fix Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -1,10 +1,12 @@
 @echo off
 
+cd /d %~dp0
+
 set DIR="."
 
 for /R %DIR% %%f in (*.cpp) do (
     echo Compiling %%f
-    g++ %%f -std=c++14 -o %%~dpnf.exe
+    g++ "%%f" -std=c++14 -o "%%~dpnf.exe"
 )
 
 echo Done.
