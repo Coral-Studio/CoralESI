216e22490efa19b42137eaf102ce13416b7d27e9
SkyNet-PKN <SkyNet0PKN@gmail.com>
2024-07-08 19:28:56 +0800

    update CESI_run.cpp

--- CESI_run/CESI_run.cpp
+++ CESI_run/CESI_run.cpp
@@ -33,31 +33,44 @@ int main(int argc, char* argv[]){
 
         PrintLog("Run command: " + _command);
 
-        int _separator_index = _command.find_first_of('|');
-        if(_separator_index == string::npos){
-            _filepath = _command;
-            _parameters = "";
-        }
-        else{
-            _filepath = _command.substr(0, _separator_index);
-            _parameters = _command.substr(_separator_index+1, (_command.size()-1)-(_separator_index+1)+1);
-        }
+        // int _separator_index = _command.find_first_of('|');
+        // if(_separator_index == string::npos){
+        //     _filepath = _command;
+        //     _parameters = "";
+        // }
+        // else{
+        //     _filepath = _command.substr(0, _separator_index);
+        //     _parameters = _command.substr(_separator_index+1, (_command.size()-1)-(_separator_index+1)+1);
+        // }
 
-        SHELLEXECUTEINFO se;
-        memset(&se, 0, sizeof(SHELLEXECUTEINFO));
-        se.cbSize = sizeof(SHELLEXECUTEINFO);
-        se.lpVerb = (TCHAR*)"runas";//Run as admin
-        se.lpFile = (TCHAR*)_filepath.c_str();
-        se.lpParameters = (TCHAR*)_parameters.c_str();
-        se.lpDirectory = NULL;//Reserved
-        se.nShow = SW_HIDE;//SW_SHOW when debugging
+        // SHELLEXECUTEINFO se;
+        // memset(&se, 0, sizeof(SHELLEXECUTEINFO));
+        // se.cbSize = sizeof(SHELLEXECUTEINFO);
+        // se.lpVerb = (TCHAR*)"runas";//Run as admin
+        // se.lpFile = (TCHAR*)_filepath.c_str();
+        // se.lpParameters = (TCHAR*)_parameters.c_str();
+        // se.lpDirectory = NULL;//Reserved
+        // se.nShow = SW_HIDE;//SW_SHOW when debugging
 
-        if(!ShellExecuteEx(&se)) PrintLog("Failed to run command: " + _command, 2);
+        // if(!ShellExecuteEx(&se)) PrintLog("Failed to run command: " + _command, 2);
         
-        Sleep(1000);
+        STARTUPINFO si={sizeof(si)};
+        PROCESS_INFORMATION pi;
+        BOOL ret = CreateProcess(NULL,(TCHAR*)_command.c_str(),NULL,NULL,FALSE,NORMAL_PRIORITY_CLASS,NULL,NULL,&si,&pi); 
+        
+        if(ret == TRUE)
+        {
+            WaitForSingleObject(pi.hThread,INFINITE); 
+            CloseHandle(pi.hThread); 
+            CloseHandle(pi.hProcess); 
+        }
+        else
+        {
+            PrintLog("Failed to run command: " + _command, 2);
+        }
     }
 
-    PrintLog("File ends without CESI_EOF_MARK mark", 1);
+    PrintLog("CESI_run.manifest ends without CESI_EOF_MARK mark", 1);
 
     return 0;
 }
